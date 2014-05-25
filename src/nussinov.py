4d752e222a97d764c10f7ba3319048995fca93f0
Jerzy <jerzybalcerzak@gmail.com>
2014-05-25 21:07:23 +0200

    simple project with unit tests example

--- src/nussinov.py
+++ src/nussinov.py
@@ -5,17 +5,27 @@ Created on May 25, 2014
 @author: jerzy
 '''
 
-# just to play with unit tests
+
 def IsOdd(n):
+    """
+     function just to play with unit tests
+    """
     return n % 2 == 1
 
 
-class Foo:
-
-    def __init__(self):
-        self.foo = 10
-        print(self.foo)
-
+class DatePattern:
+    """    
+     class just to play with unit tests
+    """
+    def __init__(self, year, month, day):
+        self.year  = year
+        self.month = month
+        self.day   = day
+    
+    def matches(self, date):
+        return ((self.year  and self.year  == date.year  or True) and
+                (self.month and self.month == date.month or True) and
+                self.day == date.day)
 
 if __name__ == '__main__':
-    Foo()
+    print "hello world"
--- src/tests1.py
+++ src/tests1.py
@@ -5,16 +5,36 @@ Created on May 25, 2014
 '''
 import unittest
 import nussinov
-
+import datetime
 
 class IsOddTests(unittest.TestCase):
-
+    """
+     example tests of IsOdd fucntion from nussinov.py module
+    """
     def testOne(self):
         self.failUnless(nussinov.IsOdd(1))
 
     def testTwo(self):
         self.failIf(nussinov.IsOdd(2))
         
+class DatePatternTests(unittest.TestCase):
+    """
+     example tests of DatePattern class from nussinov.py module
+    """
+    def testMatches(self):
+        p = nussinov.DatePattern(2004, 9, 28)
+        d = datetime.date(2004, 9, 28)
+        self.failUnless(p.matches(d))
+        
+    def testMatchesYearAsWildCard(self):
+        p = nussinov.DatePattern(0, 4, 10)
+        d = datetime.date(2005, 4, 10)
+        self.failUnless(p.matches(d))
+        
+    def testMatchesYearAndMonthAsWildCards(self):
+        p = nussinov.DatePattern(0, 0, 1)
+        d = datetime.date(2004, 10, 1)
+        self.failUnless(p.matches(d))
         
     
 if __name__ == "__main__":
