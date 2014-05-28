cbf72bce2111211c9ff3951a72b07032d991470e
Jerzy <jerzybalcerzak@gmail.com>
2014-05-28 15:02:34 +0200

    project files added

--- src/nussinov.py
+++ src/nussinov.py
@@ -4,6 +4,13 @@ Created on May 26, 2014
 @author: jerzy
 '''
 
+class NussinovError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+
 class Nussinov(object):
     '''
     main algorithm implementation
@@ -14,18 +21,21 @@ class Nussinov(object):
         '''
         Constructor
         '''
+        self.alphabet = list('ACGU')
         self.chain = chain
         self.energyMatrix = energyMatrix  
         self.chain_length = len(chain)
         if self.chain_length > 5:
-            self._initialiseSmatrix()
+            self._initialiseSMatrix()
         else:
-            print("to short sentence!") #TODO: throw exeption!
-        
+            raise NussinovError('Sequence too short!')
+
+
         
     def _initialiseSMatrix(self):
-        self._SMatrix = [[ 0 if j-i < 3 else float('nan') for j in xrange(self.chain_length)]
-                            for i in xrange(self.chain_length)]
+        self._SMatrix = [[ 0 if j-i < 3 else float('nan')
+                           for j in xrange(self.chain_length)]
+                         for i in xrange(self.chain_length)]
         
     def getSMatrix(self):
         return self._SMatrix
@@ -34,13 +44,20 @@ class Nussinov(object):
         self._buildSmatrix()
         return [(0,0)]
     
+    def _getEnergy(self, xi, xj):
+        if (xi and xj in self.alphabet):
+            return self.energyMatrix[self.alphabet.index(xi)][self.alphabet.index(xj)]
+        else:
+            raise NussinovError('Asked for an energy of not existing tuple of nucleotides!')
+        
     def _buildSmatrix(self):
-        for d in xrange(2, self.chain_length):
+        for d in xrange(3, self.chain_length):
             i = 0
+            v = []
             for j in xrange(d, self.chain_length): 
-                v = self._SMatrix[i+1][j-1] + self.energyMatrix[i][j]
-                v = max(v, self._SMatrix[i+1][j])
-                v = max(v, self._SMatrix[i][j-1])
-                v = max([v] + [self._SMatrix[i][k] + self._SMatrix[k+1][j] for k in xrange(i+1,j)] )
-                self.matrix[i][j] = v
+                v.append(self._SMatrix[i+1][j-1] + self._getEnergy(self.chain[i],self.chain[j]))
+                v.append(self._SMatrix[i+1][j])
+                v.append(self._SMatrix[i][j-1])
+                v.append(max(self._SMatrix[i][k] + self._SMatrix[k+1][j] for k in xrange(i,j-1)))
+                self._SMatrix[i][j] = max(v)
                 i += 1
--- src/tests.py
+++ src/tests.py
@@ -4,36 +4,53 @@ Created on May 26, 2014
 @author: jerzy
 '''
 import unittest
-import nussinov
+from nussinov import *
 
-
+testChain0 = 'GAAAAC'
 testChain1 = 'GGGAAAACCC'
-testChain1 = 'GAAAAC'
 
-
-
+alphabet = list('ACGU')
 testEnergyMatrix1 = [[0,0,0,2],[0,0,3,0],[0,3,0,3],[2,0,1,0]]
+testSMatrixForChain1 = [[0, 0, 0, 0, 0, 0, 0, 3, 6, 9], [0, 0, 0, 0, 0, 0, 0, 3, 6, 6], [0, 0, 0, 0, 0, 0, 0, 3, 3, 3], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
 class NussinovTests(unittest.TestCase):
    
     def test_initialiseSMatrix1(self):
-        p = nussinov.Nussinov(testChain1, testEnergyMatrix1)
+        p = Nussinov(testChain0, testEnergyMatrix1)
         p._initialiseSMatrix();
-        printMatrix(p.getSMatrix())
+#         printMatrix(p.getSMatrix())
         self.assertIsInstance(p.getSMatrix(), list)
+    def test_getEnergy1(self):
+        p = Nussinov(testChain0, testEnergyMatrix1)
+        try:
+            self.assertRaises(NussinovError, p._getEnergy('A', 'X'))
+        except NussinovError as e:
+            print 'exception occurred:\n', e.value, '\n'
+         
+    def test_getEnergy2(self):
+        p = Nussinov(testChain0, testEnergyMatrix1)
+        self.assertEqual(p._getEnergy('A','C'),0)
+        self.assertEqual(p._getEnergy('U','U'),0)
+        self.assertEqual(p._getEnergy('G','C'),3)
+      
+    def test_buildSMatrix1(self):
+        p = Nussinov(testChain1, testEnergyMatrix1)
+        p._buildSmatrix()
+        self.assertEqual(p.getSMatrix(), testSMatrixForChain1)
+        printMatrix(p.getSMatrix())
         
-        
-    def test_getPairs1(self):
-        p = nussinov.Nussinov(testChain1, testEnergyMatrix1)
-        pairs = p.getPairs();
-        self.assertIsInstance(pairs, list)
-        
-        
-    def test_getPairs2(self):
-        p = nussinov.Nussinov(testChain1, testEnergyMatrix1)
-        pairs = p.getPairs();
-        self.assertEqual(pairs,  [(0, 7), (1, 6), (2, 5), (3, 4)])
-        
+         
+#     def test_getPairs1(self):
+#         p = nussinov.Nussinov(testChain1, testEnergyMatrix1)
+#         pairs = p.getPairs();
+#         self.assertIsInstance(pairs, list)
+#          
+#          
+#     def test_getPairs2(self):
+#         p = nussinov.Nussinov(testChain1, testEnergyMatrix1)
+#         pairs = p.getPairs();
+#         self.assertEqual(pairs,  [(0, 7), (1, 6), (2, 5), (3, 4)])
+#          
 
         
 
