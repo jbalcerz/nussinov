962bb5db7b38ad6ea1e46c46b03832d639b06fbd
Oleg Druzhynets <amsterdancer@gmail.com>
2014-06-01 20:19:26 +0200

    nie zwracaj uwagi
    trochę się bawię z kodem

--- src/inputReader.py
+++ src/inputReader.py
@@ -1,8 +1,8 @@
 '''
-Created on May 26, 2014
-
-@author: jerzy
-'''
+    Created on May 26, 2014
+    
+    @author: jerzy
+    '''
 
 import sys, getopt, os
 
@@ -11,16 +11,13 @@ from itertools import permutations,  product
 
 
 class InputReader(object):
-    '''
 
-    '''
     def __init__(self,argv):
         '''
-        Constructor
-        '''
+            Constructor
+            '''
         self._helpText = 'main.py -i <inputfile> -o <outputfile> -r <raw data> \n' \
-                          '\t[-i input.txt -o output.txt]'
-        
+            '\t[-i input.txt -o output.txt]'
         
         self._dafaultInputFile = 'input.txt'
         self._dafaultOutputFile = 'output.txt'
@@ -31,22 +28,16 @@ class InputReader(object):
         except getopt.GetoptError:
             self._printHelp()
             sys.exit()
-        '''
-        if not ('-o' in opts[0]):
+        
+        try:
+            if not ('-o' in opts[0]):
+                self._handleNoOutputGiven()
+        except IndexError:
             self._handleNoOutputGiven()
 
         if opts == []:
             self._handleNoOptionsGiven()
             return
-        '''
-        if opts == []:
-            self._handleNoOptionsGiven()
-            return
-        
-        '''
-        if not ('-o' in opts[0]):
-            self._handleNoOutputGiven()
-        '''
         
         self._checkIfOptsOk(opts)
         
@@ -56,99 +47,90 @@ class InputReader(object):
             elif opt in ("-i", "--ifile"):
                 self._handleInputFileGiven(arg)
             elif opt in ("-o", "--ofile"):
-                self._handleOutputFileGiven(arg) 
+                self._handleOutputFileGiven(arg)
             elif opt in ("-r", "--rdata"):
                 self._handleRawDataGiven(arg)
+
 
     def _handleNoOutputGiven(self):
         try:
             self._outputFileObj = open(self._dafaultOutputFile, "w+")
-        except IOError as e:
-            if e.errno == 2:
-                self._handleNoInputFile("can not open default output file!")   
-                 
+        except IOError:
+            print>>sys.stderr,'Error: Can\'t open default output file.'
+            sys.exit(2);
+
+
     def _checkIfOptsOk(self,opts):
         x = []
-        for l in list(product(['-i', '--ifile'],['-r', '--rdata'])): 
+        for l in list(product(['-i', '--ifile'],['-r', '--rdata'])):
             x = x + list(permutations(l))
         if opts[0] in x:
-            raise InputReaderException(5,"-i and -r options can not be used together")
-            sys.exit(2)
-        
-
-            
-        
-
+            print>>sys.stderr,'Error: -i and -r options can not be used together'
+            sys.exit(2);
+    
+    
     def _handleInputFileGiven(self, arg):
         try:
             self._inputFileObj = open(arg)
-        except IOError as e:
-            if e.errno == 2:
-                self._handleNoInputFile("can not open given input file!")
-    
+        except IOError:
+            print>>sys.stderr,'Error: Can\'t open given input file.'
+            sys.exit(2);
+
+
     def _handleOutputFileGiven(self,arg):
         try:
             self._outputFileObj = open(arg, "w+")
-        except IOError as e:
-            if e.errno == 2:
-                self._handleNoInputFile("can not open given output file!")
-                
+        except IOError:
+            print>>sys.stderr,'Error: Can\'t open given output file.'
+            sys.exit(2);
+
     def _handleRawDataGiven(self,arg):
         try:
             self._inputFileObj = open("input.txt", "w+")
             self._inputFileObj.write(arg + "\n")
             self._inputFileObj.close()
             self._inputFileObj = open("input.txt")
-        except IOError as e:
-            if e.errno == 2:
-                self._handleNoInputFile("can not create input.txt file")
-    
+        except IOError:
+            print>>sys.stderr,'Error: Can\'t create input file \"input.txt\".'
+            sys.exit(2);
+
+
     def _handleNoOptionsGiven(self):
         try:
             self._inputFileObj = open(self._dafaultInputFile)
-            #except IOError as e:
-            #if e.errno == 2:
-            #   self._handleNoInputFile("no input file, default: input.txt")
         except IOError:
             print>>sys.stderr,'Error: No input file, using default file \"input.txt\" instead.'
             sys.exit(2);
         try:
             self._outputFileObj = open(self._dafaultOutputFile, "w+")
-            #except IOError as e:
-            #if e.errno == 2:
-            #   self._handleNoInputFile("can not open/create output.txt")
         except IOError:
-            print>>sys.stderr,'Error: Can\'t open/create output file \"input.txt\".'
+            print>>sys.stderr,'Error: Can\'t open/create output file \"output.txt\".'
             sys.exit(2);
-
-
+    
 
     def _printHelp(self):
         print(self._helpText)
         sys.exit(2)
-        
-    def _handleNoInputFile(self, message):
-        raise InputReaderException(1,message)
-        sys.exit(2)
-        
+
+
     def _check_line(self, line):
         if(len(line) < self._minimumChainLength):
-            #raise InputReaderException(2,'input string too short!')
             print>>sys.stderr,'Error: Input string is too short!'
             sys.exit(2);
         
         for i,x in enumerate(line):
             if x not in self._alphabet :
-            #raise InputReaderException(3, 'wrong symbol %s at position %d' % (x, i))
                 print>>sys.stderr,'Error: Wrong symbol \'%s\' at position %d' % (x,i)
                 sys.exit(3);
-               
-        
+    
+    
     def __iter__(self):
-        return self        
+        return self
+    
     
     def next(self):
         return self.get_one_line()
+    
     
     def get_one_line(self):
         try:
@@ -161,15 +143,17 @@ class InputReader(object):
         self._check_line(line)
         return line
     
+    
     def get_outputFileObject(self):
         try:
             self._outputFileObj
         except NameError:
-            #raise InputReaderException(4, 'output file object was not created')
             print>>sys.stderr,'Error: Output file object wasn\'t created'
             sys.exit(4);
         else:
             return self._outputFileObj
+
+
     def closeFiles(self):
         
         try:
@@ -185,24 +169,13 @@ class InputReader(object):
             pass
         else:
             self._inputFileObj.close()
-        
+
+
+
 class InputReaderException(Exception):
     def __init__(self, value, message):
         self.value = value
         self.message = message
- 
-        
-        
+    
     def __str__(self):
         return ("\nERROR (" + str(self.value) + "): " +  self.message)
-              
-        
-        
-        
-        
-        
-        
-        
-        
-    
-        
--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 '''
-Created on May 25, 2014
-
-@author: jerzy
-'''
+    Created on May 25, 2014
+    
+    @author: jerzy
+    '''
 
 import sys
 import nussinov
@@ -11,16 +11,16 @@ import inputReader
 from inputReader import InputReaderException
 
 '''
-
-'''
+    
+    '''
 
 if __name__ == '__main__':
-
-#sys.tracebacklimit=0
-     try:
+    
+    #     sys.tracebacklimit=0
+    try:
         receivedChains = inputReader.InputReader(sys.argv[1:])
-     except InputReaderException as er:
-        print(er)
+    except InputReaderException as er:
+        print er
         try:
             receivedChains
         except NameError:
@@ -28,26 +28,27 @@ if __name__ == '__main__':
         else:
             receivedChains.closeFiles()
             sys.exit(2)
-           
-       
     
-        for chain in receivedChains:
+    
+    
+    for chain in receivedChains:
         
-            print('input:'), chain
+        print 'input:', chain
         
-            energyMatrix = [[0,0,0,2],[0,0,3,0],[0,3,0,3],[2,0,1,0]]
-            x = nussinov.Nussinov(chain,energyMatrix)
-            x.calculate()
-            pairs = x.getPairs()
-            sMatrix = x.getSMatrix()
-            
-        print('output:'), pairs
+        energyMatrix = [[0,0,0,2],[0,0,3,0],[0,3,0,3],[2,0,1,0]]
+        x = nussinov.Nussinov(chain,energyMatrix)
+        x.calculate()
+        pairs = x.getPairs()
+        sMatrix = x.getSMatrix()
+        
+        print 'output:', pairs
         
         (receivedChains.get_outputFileObject()).write(str(pairs) + '\n')
-        
     
-        receivedChains.closeFiles()
-        sys.exit(0)
+    
+    receivedChains.closeFiles()
+    sys.exit(0)
+
            
 
  
